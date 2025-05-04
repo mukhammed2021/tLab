41e01d7efa0e8527e1ce8a167e7200655f4cc3a0
Mukhammed <massalimov.mukhammed@mail.ru>
2025-05-05 01:18:12 +0500

    Mobile version

--- src/widgets/header/ui/header.component.ts
+++ src/widgets/header/ui/header.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent {
+  constructor(@Inject(DOCUMENT) public doc: Document) {}
+
   public menuList = [
     {
       title: 'Продукты',
@@ -28,4 +32,7 @@ export class HeaderComponent {
       title: 'О компании',
     },
   ];
+  public toggleMenu() {
+    this.doc.documentElement.classList.toggle('menu-open');
+  }
 }
