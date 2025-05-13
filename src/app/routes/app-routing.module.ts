46ca008a2e5edf06110e4ec523ffc5fb43a1be9d
Mukhammed <massalimov.mukhammed@mail.ru>
2025-05-14 01:10:39 +0500

    Add home, about routes

--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from '../../pages/about';
+import { HomeComponent } from '../../pages/home';
 
 const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full',
   },
 ];
 
