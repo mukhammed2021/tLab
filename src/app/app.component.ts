c0e1758b0e4778b7fda2f5c4a09a0511b3d32ba2
Mukhammed <massalimov.mukhammed@mail.ru>
2025-05-14 01:12:23 +0500

    Add sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,36 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'tlab';
+export class AppComponent implements OnDestroy {
+  constructor() {
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 992px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () =>
+      this.isMobile.set(this._mobileQuery.matches);
+    if (this._mobileQuery.addEventListener) {
+      this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+    } else {
+      this._mobileQuery.addListener(this._mobileQueryListener);
+    }
+  }
+
+  public title = 'tlab';
+
+  protected readonly isMobile = signal(true);
+
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
+
+  public ngOnDestroy(): void {
+    if (this._mobileQuery.removeEventListener) {
+      this._mobileQuery.removeEventListener(
+        'change',
+        this._mobileQueryListener,
+      );
+    } else {
+      this._mobileQuery.removeListener(this._mobileQueryListener);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {
 } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { UiKitModule } from '../shared/ui';
     UiKitModule,
     HttpClientModule,
     AngularSvgIconModule.forRoot(),
+    MatSidenavModule,
   ],
   providers: [provideClientHydration(withEventReplay())],
   bootstrap: [AppComponent],
