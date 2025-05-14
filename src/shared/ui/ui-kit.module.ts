87a35e066eee66de5c3efeef25b6fbee03a8df4b
Mukhammed <massalimov.mukhammed@mail.ru>
2025-05-14 11:04:46 +0500

    Separate logo component

--- src/shared/ui/ui-kit.module.ts
+++ src/shared/ui/ui-kit.module.ts
@@ -6,15 +6,23 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { HighlightDirective } from './highlight.directive';
 import { HoverDirective } from './hover.directive';
 import { MoreInfoButtonComponent } from './more-info-button/more-info-button.component';
+import { LogoComponent } from './logo/logo.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  declarations: [HighlightDirective, HoverDirective, MoreInfoButtonComponent],
-  imports: [HttpClientModule, AngularSvgIconModule],
+  declarations: [
+    HighlightDirective,
+    HoverDirective,
+    MoreInfoButtonComponent,
+    LogoComponent,
+  ],
+  imports: [HttpClientModule, AngularSvgIconModule, RouterLink],
   exports: [
     HighlightDirective,
     HoverDirective,
     CdkAccordionModule,
     MoreInfoButtonComponent,
+    LogoComponent,
   ],
 })
 export class UiKitModule {}
